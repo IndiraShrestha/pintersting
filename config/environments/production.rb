1ac1f6d80e0c0778f5034578ec3223369dac85b3
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-08-01 09:13:32 -0400

    made changes to gem files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  #Note to set the url to your app's heroku url
+  config.action_mailer.default_url_options = { host: 'http://indira-shrestha-pintersting.herokuapp.com/' }
+
 end
